c2051ab2f8535a6212905e2e8843cda26717a1dd
akuma518 <kr.skabhi@gmail.com>
2024-06-26 19:40:31 +0530

    first commit

---
+++ src/main/java/com/project/CommentsManagement/CommentsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.CommentsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CommentsManagement/CommentsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CommentsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
